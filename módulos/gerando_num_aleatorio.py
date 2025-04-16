0177ce5bc7cb3a0e1d6bbc8be35a279532c4c53c
AndrewGReis <andrew.guedes.reis@gmail.com>
2025-04-16 13:07:19 -0300

    importando bibliotecas

---
+++ módulos/gerando_num_aleatorio.py
@@ -0,0 +1,9 @@
+# %%
+import random
+
+random.randint(1, 10)
+# %%
+#caso fosse um sorteio de nome de forma aleatoria, porém dentro de uma lista
+x=["Teo", "Andrew", "Ana", "Gustavo"]
+random.choice(x)
+# %%
---
+++ módulos/modulos_libs.py
@@ -0,0 +1,26 @@
+# calculando raiz quadrada
+
+# %%
+def sqrt(numero):
+    return numero ** (1/2)
+sqrt(9)
+# %%
+import math
+# podemos abreviar tbm usando
+# esse as é um alias, um "apelido"
+import math as mh
+pi
+
+# %%
+math.sqrt(4)
+print(math.sqrt(4))
+mh.sqrt(9)
+print(mh.sqrt(9))
+#mas vamos supor que eu nao quero importar a bliblioteca inteira
+# que por exemplo eu só quero o valor de pi
+#posso fazer assim então...
+from math import pi
+pi
+# %%
+math.e
+# %%
